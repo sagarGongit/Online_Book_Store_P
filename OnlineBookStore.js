682829573d983662c89e225bdf51b7ae55e7bc55
Sagar Bhagwatkar <148358555+sagarGongit@users.noreply.github.com>
2023-11-25 15:10:30 +0530

    Update OnlineBookStore.js

--- OnlineBookStore.js
+++ OnlineBookStore.js
@@ -1,17 +1,23 @@
-function createAuthor(name,birthYear,nationality) {
+ function createAuthor(name,birthYear,nationality) {
             this.name = name;
             this.birthYear = birthYear;
             this.nationality = nationality;
      }
-     function createBook(title,author) {
+
+     function createBook(title,author,price) {
              this.title = title;
              this.author = author;
+             this.price = price;
      }
       
       let author1 = new createAuthor("Sagar",1890,"Australian");
       let author2 = new createAuthor("Codechand",1890,"Indian");
 
-      let book1 = new createBook("The Spectacles",author1);
-      let book2 = new createBook("Coding Ke Dohe",author2);
+      let book1 = new createBook("The Spectacles",author1,600);
+      let book2 = new createBook("Coding Ke Dohe",author2,340);
 
-       console.log(book1);
+         function getInfo(book) {
+             console.log(book);
+         }
+         
+    getInfo(book1);
